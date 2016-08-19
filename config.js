6f74be9f7d4e891032597321bdb11e28d5e42fb3
Licochen <1071177578@qq.com>
2016-08-19 22:13:40 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	//mongodb: 'mongodb://localhost/technodechapter04',
-	mongodb: 'mongodb://licochen:350128chen..@ds031591.mlab.com:31591/dollars',
+	mongodb: 'mongodb://9f332af295354:76c65fc619954@192.168.1.156:49002/cba590c879324',
   robot: {
     name: '机器人',
     avatarUrl: '/images/technode.png'
