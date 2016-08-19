80b12a62e1f71284944848445a947c102d0370b4
Licochen <1071177578@qq.com>
2016-08-19 22:22:33 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 module.exports = {
 	//mongodb: 'mongodb://localhost/technodechapter04',
-	mongodb: 'mongodb://9f332af295354:76c65fc619954@192.168.1.156:49002/cba590c879324',
   robot: {
     name: '机器人',
     avatarUrl: '/images/technode.png'
